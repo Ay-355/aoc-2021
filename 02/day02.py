e88708f8c6a78ecb848d80529ecafd3ec16dda22
Ay-355 <77558710+Ay-355@users.noreply.github.com>
2021-12-02 21:30:56 -0700

    python day 2

---
+++ 02/day02.py
@@ -0,0 +1,25 @@
+def part_one(lines: list):
+    m = {"forward": 0, "down": 0, "up": 0}
+    for l in lines:
+        direction, amount = l.strip().split(" ")
+        m[direction] += int(amount)
+    return m["forward"] * (m["down"] - m["up"])
+
+
+def part_two(lines: list):
+    m = {"forward": 0, "down": 0, "up": 0}
+    depth = 0
+    for l in lines:
+        direction, amount = l.strip().split(" ")
+        m[direction] += int(amount)
+        aim = m["down"] - m["up"]
+        if (direction == "forward") and aim != 0:
+            depth += aim * int(amount)
+    return m["forward"] * depth
+
+
+if __name__ == "__main__":
+    with open("input.txt", "r") as f:
+        lines = f.read().splitlines()
+    print(f"Part One: {part_one(lines)}")
+    print(f"Part Two: {part_two(lines)}")
